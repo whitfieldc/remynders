2f065c2d40790ddabd73c7524f76645a754fc5e4
Charles <whitfield.cw@gmail.com>
2015-09-07 15:57:58 -0700

    add create action to controller and update 'new' template

--- web/controllers/reminder_controller.ex
+++ web/controllers/reminder_controller.ex
@@ -1,9 +1,24 @@
 defmodule Remynders.ReminderController do
+  alias Remynders.Reminder
   use Remynders.Web, :controller
 
   plug :action
 
   def new(conn, _params) do
-    render conn, "new.html"
+    changeset = Remynders.Reminder.changeset(%Remynders.Reminder{})
+    render conn, "new.html", changeset: changeset
   end
+
+  def create(conn, %{"reminder" => reminder_params}) do
+    changeset = Reminder.changeset(%Reminder{}, reminder_params)
+    case Repo.insert(changeset) do
+      {:ok, _reminder} ->
+        conn
+        |> put_flash(:info, "Reminder created successfully")
+        |> redirect(to: "/")
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
 end
